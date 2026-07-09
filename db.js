e552d47d787542010b5f24e8b1bf81403e8257b8
Gaurav singh <142875032+Gaurav-Singh-2526@users.noreply.github.com>
2026-07-09 19:53:52 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,10 +5,12 @@ const mongoose=require("mongoose")
 // mongodb://localhost:27017/loginData
     // mongoose.connect("mongodb+srv://Gaurav:Gaurav%4000@farmingproject.kiub9.mongodb.net/loginAuth?retryWrites=true&w=majority&appName=FarmingProject")
    // mongoose.connect("")
-    mongoose.connect("mongodb+srv://Gaurav:Gaurav87@farmingproject.kiub9.mongodb.net/loginAuth?retryWrites=true&w=majority&appName=FarmingProject")
-
-    .then(()=>console.log("database connected succesfully"))
-    .catch((error)=>console.log("Database conection error",error));
+ mongoose
+        .connect(process.env.MONGODB_URI)
+        .then(() => console.log("MongoDB Connected"))
+        .catch((err) => console.error(err))
+    .then(() => console.log("database connected succesfully"))
+    .catch((error) => console.log("Database conection error", error));
     };
   
    
